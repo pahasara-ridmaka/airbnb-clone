8dcd6cb37e98790c84b00fa0a1e3cef6ca7fd54b
Pahasara Ridmaka <pridmaka@gmail.com>
2024-08-29 12:11:35 +0530

    fix redirect url

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -31,5 +31,7 @@ export async function GET() {
       },
     });
   }
-  return NextResponse.redirect("http://localhost:3000");
+  return NextResponse.redirect(
+    "https://airbnb-clone-pahasara-ridmakas-projects.vercel.app"
+  );
 }
